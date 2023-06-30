a257e7856e01df91296e8024baed2eb1c00d1e1e
CrisLeash <pinipon.cr@gmail.com>
2023-06-30 15:19:00 +0200

    Update points.js

--- points.js
+++ points.js
@@ -1,6 +1,11 @@
 const points = document.querySelectorAll("polygon")
 for (let point of points) [
     console.log (point)
+    
+   points.getElementById("st69").style.display = "none"
 ]
 
-alert ('hellow')
+
+
+
+alert ('yo moda fukkaaas')
