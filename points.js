30c82ccde31b5adcf106d1395843380d4f108ed6
CrisLeash <pinipon.cr@gmail.com>
2023-07-05 10:37:45 +0200

    console.log(points) fixed
    next step we should find the polygons to select them individually

--- points.js
+++ points.js
@@ -1,5 +1,10 @@
 const points = document.querySelectorAll("polygon")
+for (const point of points) {
+    point.style.display ('none')
+}
 
+// check link bellow how  to call elements in SVG using path function
+// https://stackoverflow.com/questions/2753732/how-to-access-svg-elements-with-javascript
 
 console.log(points)
 
