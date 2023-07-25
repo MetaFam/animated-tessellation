bb92333a2310f3a502db9560d0c9f01e5bec3942
CrisLeash <pinipon.cr@gmail.com>
2023-07-25 14:39:07 +0200

    Visible render

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+class Point { 
+  constructor({ x, y })  {
+    this.x = x 
+    this.y = y 
+  }
+   
+  toString() {
+    return `<${this.x}, ${this.y}>`
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [tris, setTris] = useState([])
+
+  useEffect(() => {
+    const load = async () => {
+      const res = await fetch("Big Metaocto Polyart Puzzle.svg") 
+      const parser = new DOMParser();
+      const svg = parser.parseFromString(await res.text(), "application/xml");
+      console.log(svg)
+
+      const points = svg.querySelectorAll("polygon")
+      const truth = []
+      for (const point of points) {
+        const raw = point.getAttribute("points").split(/[ ,]/).map((p) => Number(p))
+        truth.push([
+          new Point({ x: raw[0], y: raw[1] }),
+          new Point({ x: raw[2], y: raw[3] }),
+          new Point({ x: raw[4], y: raw[5] }),
+        ])
+      }
+      setTris(truth)
+    }
+    load()
+  }, []) 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <svg viewBox="0 0 427.53 374.98" xmlns="http://www.w3.org/2000/svg">
+        {tris.map((tri, idx) => ( 
+          <polygon key={idx} points={
+            [tri[0].x, tri[0].y, tri[1].x, tri[1].y, tri[2].x, tri[2].y].join(" ")
+          }/>
+        ))}
+         
+      </svg>
+    </main>
   )
 }
 
